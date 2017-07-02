1b2c172bfbedc50d210a3eb63de736b7f16a0f02
Piotr Wójcik <piotr_to@op.pl>
2017-07-02 20:54:34 +0200

    observing edit view by text object

--- src/main_window.py
+++ src/main_window.py
@@ -4,8 +4,11 @@ import os
 
 from PyQt5.QtCore import QDir
 from PyQt5.QtCore import QModelIndex
+from PyQt5.QtWidgets import QAction
+from PyQt5.QtWidgets import QDockWidget
 from PyQt5.QtWidgets import QFileSystemModel
 from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMenu
 from PyQt5.QtWidgets import QPlainTextEdit
 from PyQt5.QtWidgets import QTreeView
 from PyQt5.uic import loadUi
@@ -21,28 +24,50 @@ class MainWindow(QMainWindow):
         # just for PyCharm autocomplete
         self.directory_view = QTreeView()
         self.edit_view = QPlainTextEdit()
+        self.playlist_dock = QDockWidget()
+        self.dir_dock = QDockWidget()
+        self.preview_dock = QDockWidget()
+        self.action_preview = QAction()
+        self.action_dir = QAction()
+        self.action_playlist = QAction()
+        self.menu_window = QMenu()
 
         loadUi('UI/slides.ui', self)
 
+        root_path = QDir.currentPath()
+
         self.file_model = QFileSystemModel(self)
-        self.file_model.setRootPath('')
         self.file_model.setFilter(QDir.Files | QDir.AllDirs | QDir.NoDotAndDotDot)
         self.file_model.setNameFilters(['*.sld'])
+        self.file_model.setNameFilterDisables(False)
+        self.file_model.setRootPath(root_path)
+
         self.directory_view.setModel(self.file_model)
+        self.directory_view.setRootIndex(self.file_model.index(root_path))
         self.directory_view.hideColumn(1)
         self.directory_view.hideColumn(2)
         self.directory_view.hideColumn(3)
 
-        self.text = None
+        self.menu_window.addAction(self.dir_dock.toggleViewAction())
+        self.menu_window.addAction(self.preview_dock.toggleViewAction())
+        self.menu_window.addAction(self.playlist_dock.toggleViewAction())
 
         self.screen = Screen(parent=self)
+
+        self.text = Text()
+        self.edit_view.textChanged.connect(lambda: self.text.update(self.edit_view.toPlainText()))
+
+        # TESTS
+
+
         # self.screen.show()
+        # self.screen.set_content('tekst piosenki')
 
         self.directory_view.doubleClicked.connect(self.open_file)
 
     def open_file(self, model: QModelIndex):
         path = str(self.file_model.filePath(model))
         if os.path.isfile(path) and path.endswith('.sld'):
-            self.text = Text(path)
+            with open(path, 'r') as fp:
+                self.edit_view.setPlainText(fp.read())
 
-            self.edit_view.setPlainText(self.text.text)
--- src/texts.py
+++ src/texts.py
@@ -4,16 +4,40 @@ import re
 
 
 class Text:
-    def __init__(self, file_path):
-        self._path = file_path
-
-        with open(self._path, 'r') as fp:
-            self._text = fp.read()
-
-        self._pages = re.split('---+', self._text)
+    def __init__(self):
+        self._path = None
+        self._current_index = 0
 
     def __getitem__(self, item):
         return self._pages[item]
+
+    @property
+    def current_page(self):
+        return self[self._current_index]
+
+    @property
+    def next(self):
+        if self._current_index+1 == len(self._pages):
+            raise StopIteration()
+
+        page = self.current_page
+        self._current_index += 1
+        return page
+
+    @property
+    def previous(self):
+        if self._current_index == 0:
+            raise StopIteration()
+
+        page = self.current_page
+        self._current_index -= 1
+        return page
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.next
 
     @property
     def text(self):
@@ -21,9 +45,13 @@ class Text:
 
     @text.setter
     def text(self, text):
-        self.update(text)
+        self._text = text
+        self._pages = re.split('---+', self._text)
 
     def update(self, text):
-        self._text = text
-        with open(self._path, 'w') as fp:
-            fp.write(text)
+        self.text = text
+
+    def save(self):
+        if self._path:
+            with open(self._path, 'w') as fp:
+                fp.write(self._text)
